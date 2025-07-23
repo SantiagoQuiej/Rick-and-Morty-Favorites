f1299d8c00e25195cfa04d610fc1b1e12e91408d
Lucas Quiej <quiej7959@gmail.com>
2025-07-23 10:08:13 -0600

    Rick and Morty Favorite

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,6 +1,9 @@
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useContext } from 'react';
+import { Container, Nav, Navbar, Badge } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom';
+import { usercontext } from '../Context/FavoritesContext';
 const Menu = () => {
+    const { favoritos } = useContext(usercontext)
     return (
         <>
             <Navbar expand="lg" className="bg-body-tertiary">
@@ -8,8 +11,10 @@ const Menu = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <NavLink className={({ isActive }) => isActive ? 'active text-danger nav-link' : 'nav-link'} to='/'>Lista de personajes</NavLink>
-                            <NavLink className={({ isActive }) => isActive ? 'active text-danger nav-link' : 'nav-link'} to='/favoritos'>Favoritos</NavLink>
+                            <NavLink className={({ isActive }) => isActive ? 'active text-primary nav-link' : 'nav-link'} to='/'>Lista de personajes</NavLink>
+                            <NavLink className={({ isActive }) => isActive ? 'active text-primary nav-link' : 'nav-link'} to='/favoritos'>Favoritos
+                                <Badge bg="danger" className='m-1 rounded-5'>{favoritos.length}</Badge>
+                            </NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
